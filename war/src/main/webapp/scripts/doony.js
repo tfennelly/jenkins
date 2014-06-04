81188096114980d002cc0b47f1cd8a7964a4a7f9
tfennelly <tom.fennelly@gmail.com>
2014-06-04 14:49:22 +0100

    Fied 'nobuilt' build orbs

--- war/src/main/webapp/scripts/doony.js
+++ war/src/main/webapp/scripts/doony.js
@@ -583,11 +583,13 @@ jQuery(function($) {
     setInterval(function() {
         replaceBouncingFloatyBall("img[src*='red_anime.gif']", '#d9534f');
         replaceBouncingFloatyBall("img[src*='blue_anime.gif']", green);
+        replaceBouncingFloatyBall("img[src*='nobuilt_anime.gif']", '#999');
         replaceBouncingFloatyBall("img[src*='grey_anime.gif']", '#999');
         replaceBouncingFloatyBall("img[src*='aborted_anime.gif']", '#999');
         replaceBouncingFloatyBall("img[src*='yellow_anime.gif']", '#f0ad4e');
     }, 10);
     setInterval(function() {
+        replaceFloatyBall("img[src*='/nobuilt.png']", "nobuilt");
         replaceFloatyBall("img[src*='/grey.png']", "aborted");
         replaceFloatyBall("img[src*='/aborted.png']", "aborted");
         replaceFloatyBall("img[src*='/blue.png']", "success");
