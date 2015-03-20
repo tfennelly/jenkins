2b15c74e5a71a7378fa00377612772652402ba06
tfennelly <tom.fennelly@gmail.com>
2015-03-20 07:53:02 +0000

    function rename

--- core/src/main/java/hudson/widgets/HistoryPageFilter.java
+++ core/src/main/java/hudson/widgets/HistoryPageFilter.java
@@ -99,8 +99,8 @@ public class HistoryPageFilter<T> {
             if (toFillCount > 0) {
                 // Locate the point in the items list where the 'newerThan' build item is. Once located,
                 // add a max of 'getFillCount' build items before that build item.
-                long newestInList = HistoryPageEntry.getQueueId(items.get(0));
-                long oldestInList = HistoryPageEntry.getQueueId(items.get(items.size() - 1));
+                long newestInList = HistoryPageEntry.getEntryId(items.get(0));
+                long oldestInList = HistoryPageEntry.getEntryId(items.get(items.size() - 1));
                 int newerThanIdx = -1;
 
                 if (newerThan > newestInList) {
@@ -110,7 +110,7 @@ public class HistoryPageFilter<T> {
                     // go through the list and locate the cut-off point.
                     for (int i = 0; i < items.size(); i++) {
                         T item = items.get(i);
-                        if (HistoryPageEntry.getQueueId(item) <= newerThan) {
+                        if (HistoryPageEntry.getEntryId(item) <= newerThan) {
                             newerThanIdx = i;
                             break;
                         }
@@ -145,7 +145,7 @@ public class HistoryPageFilter<T> {
         } else if (olderThan != null) {
             for (int i = 0; i < items.size(); i++) {
                 T item = items.get(i);
-                if (HistoryPageEntry.getQueueId(item) >= olderThan) {
+                if (HistoryPageEntry.getEntryId(item) >= olderThan) {
                     hasUpPage = true;
                 } else {
                     add(item);
@@ -164,12 +164,11 @@ public class HistoryPageFilter<T> {
         // Queue items can start building out of order with how they got added to the queue. Sorting them
         // before adding to the page. They'll still get displayed before the building items coz they end
         // up in a different list in HistoryPageFilter.
-        final HistoryPageFilter thisPage = this;
         Collections.sort(items, new Comparator<T>() {
             @Override
             public int compare(T o1, T o2) {
-                long o1QID = HistoryPageEntry.getQueueId(o1);
-                long o2QID = HistoryPageEntry.getQueueId(o2);
+                long o1QID = HistoryPageEntry.getEntryId(o1);
+                long o2QID = HistoryPageEntry.getEntryId(o2);
 
                 if (o1QID < o2QID) {
                     return 1;
@@ -193,7 +192,7 @@ public class HistoryPageFilter<T> {
         }
 
         // TODO maybe this should be an error?
-        return HistoryPageEntry.getQueueId(entry) + 1;
+        return HistoryPageEntry.getEntryId(entry) + 1;
     }
 
     private void addQueueItem(Queue.Item item) {
