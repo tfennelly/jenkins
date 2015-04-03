de11b6e5af406fce29003edb240230af1897a3fa
tfennelly <tom.fennelly@gmail.com>
2015-04-03 14:38:42 +0100

    Fix indentation ... what is screwing this up?

--- core/src/main/java/hudson/widgets/HistoryPageFilter.java
+++ core/src/main/java/hudson/widgets/HistoryPageFilter.java
@@ -61,181 +61,183 @@ public class HistoryPageFilter<T> {
      * Create a history page filter instance.
      *
      * @param maxEntries The max number of entries allowed for the page.
-     * @param newerThan Queue IDs newer/greater than this queue ID take precedence on this page. <strong>Should not be set
-     *                  if setting the {@code olderThan} parameter.</strong>
-     * @param olderThan Queue IDs older/less than this queue ID take precedence on this page. <strong>Should not be set if
-     *                  setting the {@code newerThan} parameter (will be ignored).</strong>
+     * @param newerThan  Queue IDs newer/greater than this queue ID take precedence on this page. <strong>Should not be set
+     *                   if setting the {@code olderThan} parameter.</strong>
+     * @param olderThan  Queue IDs older/less than this queue ID take precedence on this page. <strong>Should not be set if
+     *                   setting the {@code newerThan} parameter (will be ignored).</strong>
      */
     public HistoryPageFilter(int maxEntries, Long newerThan, Long olderThan) {
-	this.maxEntries = maxEntries;
-	this.newerThan = newerThan;
-	this.olderThan = olderThan;
+        this.maxEntries = maxEntries;
+        this.newerThan = newerThan;
+        this.olderThan = olderThan;
     }
 
     /**
      * Add build items to the History page.
+     *
      * @param items The items to be added. Assumes the list of items are in descending queue ID order i.e. newest first.
      */
     public void add(@Nonnull List<T> items) {
-	if (items.isEmpty()) {
-	    return;
-	}
+        if (items.isEmpty()) {
+            return;
+        }
 
-	sort(items);
+        sort(items);
 
-	nextBuildNumber = getNextBuildNumber(items.get(0));
+        nextBuildNumber = getNextBuildNumber(items.get(0));
 
-	if (newerThan == null && olderThan == null) {
-	    // Just return the first page of entries (newest)
-	    for (T item : items) {
-		add(item);
-		if (isFull()) {
-		    break;
-		}
-	    }
-	    hasDownPage = (items.size() > maxEntries);
-	} else if (newerThan != null) {
-	    int toFillCount = getFillCount();
-	    if (toFillCount > 0) {
-		// Locate the point in the items list where the 'newerThan' build item is. Once located,
-		// add a max of 'getFillCount' build items before that build item.
-		long newestInList = HistoryPageEntry.getEntryId(items.get(0));
-		long oldestInList = HistoryPageEntry.getEntryId(items.get(items.size() - 1));
-		int newerThanIdx = -1;
+        if (newerThan == null && olderThan == null) {
+            // Just return the first page of entries (newest)
+            for (T item : items) {
+                add(item);
+                if (isFull()) {
+                    break;
+                }
+            }
+            hasDownPage = (items.size() > maxEntries);
+        } else if (newerThan != null) {
+            int toFillCount = getFillCount();
+            if (toFillCount > 0) {
+                // Locate the point in the items list where the 'newerThan' build item is. Once located,
+                // add a max of 'getFillCount' build items before that build item.
+                long newestInList = HistoryPageEntry.getEntryId(items.get(0));
+                long oldestInList = HistoryPageEntry.getEntryId(items.get(items.size() - 1));
+                int newerThanIdx = -1;
 
-		if (newerThan > newestInList) {
-		    // Nothing newer
-		} else if (newerThan >= oldestInList) {
-		    // newerThan is within the range of items in the item list.
-		    // go through the list and locate the cut-off point.
-		    for (int i = 0; i < items.size(); i++) {
-			T item = items.get(i);
-			if (HistoryPageEntry.getEntryId(item) <= newerThan) {
-			    newerThanIdx = i;
-			    break;
-			}
-		    }
-		} else if (newerThan < oldestInList) {
-		    newerThanIdx = items.size();
-		}
+                if (newerThan > newestInList) {
+                    // Nothing newer
+                } else if (newerThan >= oldestInList) {
+                    // newerThan is within the range of items in the item list.
+                    // go through the list and locate the cut-off point.
+                    for (int i = 0; i < items.size(); i++) {
+                        T item = items.get(i);
+                        if (HistoryPageEntry.getEntryId(item) <= newerThan) {
+                            newerThanIdx = i;
+                            break;
+                        }
+                    }
+                } else if (newerThan < oldestInList) {
+                    newerThanIdx = items.size();
+                }
 
-		if (newerThanIdx != -1) {
-		    if (newerThanIdx <= maxEntries) {
-			// If there's less than a full page of items newer than "newerThan", then it's ok to
-			// fill the page with items older than "newerThan".
-			int itemCountToAdd = Math.min(toFillCount, items.size());
-			for (int i = 0; i < itemCountToAdd; i++) {
-			    add(items.get(i));
-			}
-		    } else {
-			// There's more than a full page of items newer than "newerThan".
-			for (int i = (newerThanIdx - toFillCount); i < newerThanIdx; i++) {
-			    add(items.get(i));
-			}
-			hasUpPage = true;
-		    }
-		    // if there are items after the "newerThan" item, then we
-		    // can page down.
-		    hasDownPage = (items.size() > newerThanIdx + 1);
-		} else {
-		    // All builds are older than newerThan ?
-		    hasDownPage = true;
-		}
-	    }
-	} else if (olderThan != null) {
-	    for (int i = 0; i < items.size(); i++) {
-		T item = items.get(i);
-		if (HistoryPageEntry.getEntryId(item) >= olderThan) {
-		    hasUpPage = true;
-		} else {
-		    add(item);
-		    if (isFull()) {
-			// This page is full but there may be more builds older
-			// than the oldest on this page.
-			hasDownPage = (i + 1 < items.size());
-			break;
-		    }
-		}
-	    }
-	}
+                if (newerThanIdx != -1) {
+                    if (newerThanIdx <= maxEntries) {
+                        // If there's less than a full page of items newer than "newerThan", then it's ok to
+                        // fill the page with items older than "newerThan".
+                        int itemCountToAdd = Math.min(toFillCount, items.size());
+                        for (int i = 0; i < itemCountToAdd; i++) {
+                            add(items.get(i));
+                        }
+                    } else {
+                        // There's more than a full page of items newer than "newerThan".
+                        for (int i = (newerThanIdx - toFillCount); i < newerThanIdx; i++) {
+                            add(items.get(i));
+                        }
+                        hasUpPage = true;
+                    }
+                    // if there are items after the "newerThan" item, then we
+                    // can page down.
+                    hasDownPage = (items.size() > newerThanIdx + 1);
+                } else {
+                    // All builds are older than newerThan ?
+                    hasDownPage = true;
+                }
+            }
+        } else if (olderThan != null) {
+            for (int i = 0; i < items.size(); i++) {
+                T item = items.get(i);
+                if (HistoryPageEntry.getEntryId(item) >= olderThan) {
+                    hasUpPage = true;
+                } else {
+                    add(item);
+                    if (isFull()) {
+                        // This page is full but there may be more builds older
+                        // than the oldest on this page.
+                        hasDownPage = (i + 1 < items.size());
+                        break;
+                    }
+                }
+            }
+        }
     }
 
     public int size() {
-	return queueItems.size() + runs.size();
+        return queueItems.size() + runs.size();
     }
 
     private void sort(List<T> items) {
-	// Queue items can start building out of order with how they got added to the queue. Sorting them
-	// before adding to the page. They'll still get displayed before the building items coz they end
-	// up in a different list in HistoryPageFilter.
-	Collections.sort(items, new Comparator<T>() {
-	    @Override
-	    public int compare(T o1, T o2) {
-		long o1QID = HistoryPageEntry.getEntryId(o1);
-		long o2QID = HistoryPageEntry.getEntryId(o2);
+        // Queue items can start building out of order with how they got added to the queue. Sorting them
+        // before adding to the page. They'll still get displayed before the building items coz they end
+        // up in a different list in HistoryPageFilter.
+        Collections.sort(items, new Comparator<T>() {
+            @Override
+            public int compare(T o1, T o2) {
+                long o1QID = HistoryPageEntry.getEntryId(o1);
+                long o2QID = HistoryPageEntry.getEntryId(o2);
 
-		if (o1QID < o2QID) {
-		    return 1;
-		} else if (o1QID == o2QID) {
-		    return 0;
-		} else {
-		    return -1;
-		}
-	    }
-	});
+                if (o1QID < o2QID) {
+                    return 1;
+                } else if (o1QID == o2QID) {
+                    return 0;
+                } else {
+                    return -1;
+                }
+            }
+        });
     }
 
     private long getNextBuildNumber(@Nonnull T entry) {
-	if (entry instanceof Queue.Item) {
-	    Queue.Task task = ((Queue.Item) entry).task;
-	    if (task instanceof Job) {
-		return ((Job) task).getNextBuildNumber();
-	    }
-	} else if (entry instanceof Run) {
-	    return ((Run) entry).getParent().getNextBuildNumber();
-	}
+        if (entry instanceof Queue.Item) {
+            Queue.Task task = ((Queue.Item) entry).task;
+            if (task instanceof Job) {
+                return ((Job) task).getNextBuildNumber();
+            }
+        } else if (entry instanceof Run) {
+            return ((Run) entry).getParent().getNextBuildNumber();
+        }
 
-	// TODO maybe this should be an error?
-	return HistoryPageEntry.getEntryId(entry) + 1;
+        // TODO maybe this should be an error?
+        return HistoryPageEntry.getEntryId(entry) + 1;
     }
 
     private void addQueueItem(Queue.Item item) {
-	updateNewestOldest(item.getId());
-	queueItems.add(new HistoryPageEntry(item));
+        updateNewestOldest(item.getId());
+        queueItems.add(new HistoryPageEntry(item));
     }
 
     private void addRun(Run run) {
-	updateNewestOldest(run.getQueueId());
-	runs.add(new HistoryPageEntry(run));
+        updateNewestOldest(run.getQueueId());
+        runs.add(new HistoryPageEntry(run));
     }
 
     private void updateNewestOldest(long queueId) {
-	newestOnPage = Math.max(newestOnPage, queueId);
-	oldestOnPage = Math.min(oldestOnPage, queueId);
+        newestOnPage = Math.max(newestOnPage, queueId);
+        oldestOnPage = Math.min(oldestOnPage, queueId);
     }
 
     private boolean add(T entry) {
-	// Purposely not calling isFull(). May need to add a greater number of entries
-	// to the page initially, newerThan then cutting it back down to size using cutLeading()
-	if (entry instanceof Queue.Item) {
-	    addQueueItem((Queue.Item) entry);
-	    return true;
-	} else if (entry instanceof Run) {
-	    addRun((Run) entry);
-	    return true;
-	}
-	return false;
+        // Purposely not calling isFull(). May need to add a greater number of entries
+        // to the page initially, newerThan then cutting it back down to size using cutLeading()
+        if (entry instanceof Queue.Item) {
+            addQueueItem((Queue.Item) entry);
+            return true;
+        } else if (entry instanceof Run) {
+            addRun((Run) entry);
+            return true;
+        }
+        return false;
     }
 
     private boolean isFull() {
-	return (size() >= maxEntries);
+        return (size() >= maxEntries);
     }
 
     /**
      * Get the number of items required to fill the page.
+     *
      * @return The number of items required to fill the page.
      */
     private int getFillCount() {
-	return Math.max(0, (maxEntries - size()));
+        return Math.max(0, (maxEntries - size()));
     }
 }
